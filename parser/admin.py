16f9f426830c3510f60ca71d4e57ede633a32641
Vlasov R. V <radislavvlasov@gmail.com>
2023-06-05 18:16:40 +0300

    feat: добавлена модель прогресса парсинга

--- parser/admin.py
+++ parser/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    model: models.ProjectModel
+
+
+class ParsingAdmin(ProjectAdmin):
+    model = models.Parsing
+    list_display = ("start_time", "current", "all")
+
+
+def register_models():
+    models_with_admin_page = ProjectAdmin.__subclasses__()
+
+    for admin_model in models_with_admin_page:
+        admin.site.register(admin_model.model, admin_model)
+
+    for model in [x for x in models.ProjectModel.__subclasses__() if
+                  x not in [y.model for y in models_with_admin_page]]:
+        admin.site.register(model)
+
+
+register_models()
--- parser/models.py
+++ parser/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class ProjectModel(models.Model):
+    class Meta:
+        abstract = True
+
+
+class Parsing(ProjectModel):
+    current = models.IntegerField("Текущий шаг парсинга")
+    all = models.IntegerField("Общее количество шагов парсинга")
+    start_time = models.DateTimeField("Время начала парсинга", auto_now = True)
+
+    def __str__(self) -> str:
+        return str(self.start_time)
