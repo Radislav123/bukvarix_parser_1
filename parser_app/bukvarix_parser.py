521d311b27dc487050bf37f6ff73589210ee609b
Vlasov R. V <radislavvlasov@gmail.com>
2023-06-07 17:38:31 +0300

    refactor: попытка развернуть на другом хостинге №2

--- parser_app/bukvarix_parser.py
+++ parser_app/bukvarix_parser.py
@@ -32,6 +32,7 @@ class BukvarixParser:
         options = ChromeOptions()
         # этот параметр тоже нужен, так как в режиме headless с некоторыми элементами нельзя взаимодействовать
         options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument("--no-sandbox")
         options.add_argument("--headless")
         options.add_argument("--window-size=1920,1080")
         options.add_experimental_option("excludeSwitches", ["enable-logging"])
